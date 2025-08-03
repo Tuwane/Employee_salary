c344b60c92771a6cf1eae84bb7fe3f7f3467de6b
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-08-03 22:10:49 +0200

    first commit

---
+++ worker.java
@@ -0,0 +1,32 @@
+public class worker{
+private int empNum;
+private String lastName;
+private String firstName;
+private double salary;
+
+public void setEmpNum(int empNum){
+	this.empNum = empNum;
+}
+public int getEmpNum(){
+	return empNum;
+}
+public void setlastName(String surn){
+	lastName = surn;
+}
+public String getlastName(){
+	return lastName;
+}
+public void setfirstName(String name){
+	lastName = name;
+}
+public String getfirstName(){
+	return lastName;
+}
+public void setSalary(double sal){
+	salary = sal;
+}
+public double getSalary(){
+	return salary;
+}
+
+}
